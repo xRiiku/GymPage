c3f220e6d3c35b0ab41cb96a6cc83b2e91f0acf4
RikuDev <riku@rikudev.com>
2023-05-14 12:48:30 +0200

    a

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,12 @@
-import bgImg from '../assets/img/home-bg.jpg'
+import WhyChose from './HomeSections/WhyChose'
+import Intro from './HomeSections/intro'
 
 export default function Home () {
 
     return (
-    <div className='relative'>
-        <div className='relative inset-0'>
-            <img
-            className='w-full h-full object-cover'
-            src={bgImg}
-            alt='background'
-            />
-        </div>
-
-        <div className='container absolute inset-0 flex justify-end items-center'>
-        <div className='flex-col flex text-left'>
-            <h3 className='uppercase text-3xl text-white'>Shape your body</h3>
-            <h1 className='text-white text-6xl font-bold uppercase mt-5'>Be <span className='text-orange-600'>Strong</span></h1>
-            <h1 className='text-white text-6xl font-bold uppercase mt-2'>Training hard</h1>
-            <button className='bg-orange-600 text-white text-2xl uppercase h-10 w-32 mt-7 hover:bg-orange-500'>Get info</button>
-        </div>
-        </div>
+        <div className='parent'>
+            <Intro />
+            <WhyChose />
     </div>
     )
 }
---
+++ src/Pages/HomeSections/WhyChose.jsx
@@ -0,0 +1,16 @@
+export default function WhyChose(){
+
+    return(
+        <div className='flex justify-center items-center p-20'>
+
+            <div className='flex flex-col justify-center items-center'>
+                <span className='text-orange-500 uppercase text-2xl font-bold'>Why chose us?</span>
+                <span className='text-white text-4xl uppercase mt-3'>Push your limits forward</span>
+            </div>
+
+            <div className='parent'>
+            
+            </div>
+        </div>
+    )
+}
