afb906d1c712132681751952bc1dfc5b06c8eef1
RikuDev <riku@rikudev.com>
2023-05-24 20:01:52 +0200

    deleted team css and did on tailwind

--- src/Pages/Sections/OurTeamTrainers.jsx
+++ src/Pages/Sections/OurTeamTrainers.jsx
@@ -1,7 +1,3 @@
-
-import '../../assets/utilities/team.css'; // Importa el archivo CSS que contiene los estilos
-
-// Importa las imágenes
 import img1 from '../../assets/img/team/team-1.jpg';
 import img2 from '../../assets/img/team/team-2.jpg';
 import img3 from '../../assets/img/team/team-3.jpg';
@@ -21,67 +17,67 @@ export default function OurTeamTrainers() {
         {/* CARD PARENT (Controls all 6 cards) */}
         <div className='cardParent place-content-between gap-10 mt-16 grid grid-cols-3 maxlg:grid-cols-2 maxsm:grid-cols-1'>
           {/* CARD info (1)*/}
-          <div className='relative flex flex-col justify-center items-center teamCard cursor pointer'>
+          <div className='relative flex flex-col justify-center items-center cursor-pointer'>
             <div>
                 <img className='rounded-2xl' src={img1} alt='testimonial 1'></img>
-            <div className='overlay'>
+            <div className='absolute h-full flex flex-col justify-center items-center text-center bottom-0 left-0 right-0 bg-black bg-opacity-70 opacity-0 transition-opacity ease-in hover:opacity-100'>
                 <span className='text-white mt-8 text-2xl font-bold maxls:text-lg maxxs:text-sm'>Isabella</span>
-                <p className='text-white mt-5 pChoseWidth maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
+                <p className='text-white mt-5 pChoseWidthTeam maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
             </div>
             </div>
         </div>
 
           {/* CARD info (2)*/}
-          <div className='relative flex flex-col justify-center items-center teamCard cursor-pointer'>
+          <div className='relative flex flex-col justify-center items-center cursor-pointer'>
             <div>
                 <img className='rounded-2xl' src={img2} alt='testimonial 2'></img>
-            <div className='overlay'>
+            <div className='absolute h-full flex flex-col justify-center items-center text-center bottom-0 left-0 right-0 bg-black bg-opacity-70 opacity-0 transition-opacity ease-in hover:opacity-100'>
                 <span className='text-white mt-8 text-2xl font-bold maxls:text-lg maxxs:text-sm'>Alexander</span>
-                <p className='text-white mt-5 pChoseWidth maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
+                <p className='text-white mt-5 pChoseWidthTeam maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
             </div>
             </div>
         </div>
 
           {/* CARD info (3)*/}
-          <div className='relative flex flex-col justify-center items-center teamCard cursor-pointer'>
+          <div className='relative flex flex-col justify-center items-center cursor-pointer'>
             <div>
                 <img className='rounded-2xl' src={img3} alt='testimonial 3'></img>
-            <div className='overlay'>
+            <div className='absolute h-full flex flex-col justify-center items-center text-center bottom-0 left-0 right-0 bg-black bg-opacity-70 opacity-0 transition-opacity ease-in hover:opacity-100'>
                 <span className='text-white mt-8 text-2xl font-bold maxls:text-lg maxxs:text-sm'>Benjamin</span>
-                <p className='text-white mt-5 pChoseWidth maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
+                <p className='text-white mt-5 pChoseWidthTeam maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
             </div>
             </div>
         </div>
 
           {/* CARD info (4)*/}
-          <div className='relative flex flex-col justify-center items-center teamCard cursor-pointer'>
+          <div className='relative flex flex-col justify-center items-center cursor-pointer'>
             <div>
                 <img className='rounded-2xl' src={img4} alt='testimonial 4'></img>
-            <div className='overlay'>
-                <span className='text-white mt-8 text-2xl font-bold maxls:text-lg maxxs:text-sm'>Benjamin</span>
-                <p className='text-white mt-5 pChoseWidth maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
+            <div className='absolute h-full flex flex-col justify-center items-center text-center bottom-0 left-0 right-0 bg-black bg-opacity-70 opacity-0 transition-opacity ease-in hover:opacity-100'>
+                <span className='text-white mt-8 text-2xl font-bold maxls:text-lg maxxs:text-sm'>Brandon</span>
+                <p className='text-white mt-5 pChoseWidthTeam maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
             </div>
             </div>
         </div>
 
           {/* CARD info (5)*/}
-          <div className='relative flex flex-col justify-center items-center teamCard cursor-pointer'>
+          <div className='relative flex flex-col justify-center items-center cursor-pointer'>
             <div>
                 <img className='rounded-2xl' src={img5} alt='testimonial 5'></img>
-            <div className='overlay'>
+            <div className='absolute h-full flex flex-col justify-center items-center text-center bottom-0 left-0 right-0 bg-black bg-opacity-70 opacity-0 transition-opacity ease-in hover:opacity-100'>
                 <span className='text-white mt-8 text-2xl font-bold maxls:text-lg maxxs:text-sm'>Sophia</span>
-                <p className='text-white mt-5 pChoseWidth maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
+                <p className='text-white mt-5 pChoseWidthTeam maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
             </div>
             </div>
         </div>
 
           {/* CARD info (6)*/}
-          <div className='relative flex flex-col justify-center items-center teamCard cursor-pointer'>
+          <div className='relative flex flex-col justify-center items-center cursor-pointer'>
             <div>
                 <img className='rounded-2xl' src={img6} alt='testimonial 6'></img>
-            <div className='overlay'>
+            <div className='absolute h-full flex flex-col justify-center items-center text-center bottom-0 left-0 right-0 bg-black bg-opacity-70 opacity-0 transition-opacity ease-in hover:opacity-100'>
                 <span className='text-white mt-8 text-2xl font-bold maxls:text-lg maxxs:text-sm'>Evelyn</span>
-                <p className='text-white mt-5 pChoseWidth maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
+                <p className='text-white mt-5 pChoseWidthTeam maxls:text-sm maxxs:text-xs'>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
             </div>
             </div>
         </div>
