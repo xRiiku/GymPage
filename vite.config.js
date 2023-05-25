707847718ea4789bd82ea82fc55eeab3a9547473
RikuDev <riku@rikudev.com>
2023-05-25 17:07:15 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://rikudev.com/GymPage', // Configura la ruta base de tu aplicación
+  build: {
+    outDir: 'dist', // Configura la carpeta de salida para los archivos generados por el comando npm run build
+  },
 })
